1668fd9f41f4ca33b133f617c0fbbce8d163984a
forsaken1 <alexey2142@mail.ru>
2018-10-08 20:40:55 +1000

    add fixes for proc from other fork

--- lib/cashier/railtie.rb
+++ lib/cashier/railtie.rb
@@ -1,5 +1,11 @@
 module Cashier
   class Railtie < ::Rails::Railtie
     config.cashier = Cashier
+
+    initializer "cashier.active_support.cache.instrumentation" do |app|
+      ActiveSupport.on_load(:action_controller) do
+        require 'cashier/controllers/application_controller'
+      end
+   end
   end
 end
