556b363ca145f18c1316ba75f9e9b9cb17642a6e
Alexey Krylov <alexey2142@mail.ru>
2018-10-08 22:09:01 +1000

    Fix proc (#1)
    * add fixes for proc from other fork
    
    * update
    
    * remove instrument

--- lib/cashier/railtie.rb
+++ lib/cashier/railtie.rb
@@ -1,5 +1,13 @@
 module Cashier
   class Railtie < ::Rails::Railtie
     config.cashier = Cashier
+
+    initializer "cashier.active_support.cache.instrumentation" do |app|
+      ActiveSupport.on_load(:action_controller) do
+        require "cashier/action_controller_methods"
+
+        ::ActionController::Base.send(:include, Cashier::ActionControllerMethods)
+      end
+   end
   end
 end
